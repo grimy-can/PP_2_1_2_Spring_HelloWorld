5d85c97226bd37d5d9b71b9f05a159cd55fe71b7
Grimy Can <greyhead@bk.ru>
2023-09-08 19:43:44 +0500

    Задача 2.1.3 решение 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,20 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.printf("bean1 message: %s\n", bean1.getMessage());
+        System.out.printf("bean2 message: %s\n", bean2.getMessage());
+        System.out.printf("bean1 = bean2: %s\n", bean1 == bean2);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.printf("cat1 message: %s\n", cat1.getVoice());
+        System.out.printf("cat2 message: %s\n", cat2.getVoice());
+        System.out.printf("cat1 = cat2: %s\n", cat1 == cat2);
     }
 }
